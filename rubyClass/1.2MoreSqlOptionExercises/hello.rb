5e097e5636b01be90ababde3b983c67ed1a2f183
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-10-23 15:44:54 +0300

    ruby intor

--- rubyClass/1.2MoreSqlOptionExercises/hello.rb
+++ rubyClass/1.2MoreSqlOptionExercises/hello.rb
@@ -1,2 +1,11 @@
 puts "Hello Ruby"
 # puts = put string 
+#  gets is waiting input like prompt
+
+# Call puts with a string containing a newline character. Then call puts with a string containing a tab character. Finally, call puts a third time with a string containing a " double quotation mark.
+
+#  puts "hello   /n hi"
+# puts "hello/t"
+# puts '"hello" he said'
+
+# typeof yerine .class kulllaniliyor Ruby de  
